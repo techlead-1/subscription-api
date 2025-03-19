dc15f3dcfa2f0f5f4797d72bd7cd1712d38fb497
techlead <adesanya1ademola@gmail.com>
2025-03-19 18:24:22 +0100

    Added the routes to app.js and route prefix

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { PORT} from "./config/env.js";
 
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = express();
 const port = PORT
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/users', userRouter);
 
 app.get('/', (req, res) => {
     res.send("Weclome to my subscription api");
