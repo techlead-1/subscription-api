916c815724d65ebf84fad123cd5e42c3d1e38bcb
techlead <adesanya1ademola@gmail.com>
2025-03-21 04:53:12 +0100

    used the middleware in app.js and other default middlewares

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = PORT
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use(errorMiddleware);
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to my subscription api");
